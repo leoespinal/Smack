2857e4e66f69eaca2ed674618d40d3dcf0c78c20
Leo Espinal <leo.espinal420@gmail.com>
2018-03-27 11:22:57 -0400

    Added LoginVC UI

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,23 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Leo Espinal on 3/27/18.
+//  Copyright © 2018 Leo Espinal. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    @IBAction func closePressed(_ sender: Any) {
+        //Dismiss this view
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Leo Espinal on 3/27/18.
+//  Copyright © 2018 Leo Espinal. All rights reserved.
+//
+
+import Foundation
+
+//Segues
+let TO_LOGIN = "toLogin"
